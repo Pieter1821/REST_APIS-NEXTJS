08b1499f73a1e13540c4b7d0ea29d986ae36e753
Pieter Deane <deanepieter@gmail.com>
2024-09-17 07:05:59 +0200

    Add PATCH route for updating user information
    - Implemented PATCH route to update user's username based on userId.
    - Added error handling for missing or invalid userId and newUsername.
    - Included database connection and user update logic.
    - Improved error messages for better debugging.

--- app/api/(Auth)/users/route.ts
+++ app/api/(Auth)/users/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
 import connect from '@/lib/db';
 import User from '@/lib/modals/user';
+import { Types } from 'mongoose';
+import {ObjectId} from 'mongodb';
+
+
 
 export const GET = async () => {
     try {
@@ -18,12 +22,50 @@ export const POST = async (request: Request) => {
         await connect();
         const newUser = new User(body);
         await newUser.save();
-        return new NextResponse(JSON.stringify({message: "User created" , user: newUser}), { status: 200 });
+        return new NextResponse(JSON.stringify({ message: "User created", user: newUser }), { status: 200 });
 
     } catch (error) {
         return new NextResponse("Error in fetching users: " + (error instanceof Error ? error.message : "Unknown error"), { status: 500 });
     }
 };
+
+export const PATCH = async (request: Request) => {
+    try {
+        const body = await request.json();
+        const { userId, newUsername } = body;
+        await connect();
+        if (!userId || !newUsername) {
+            return new NextResponse(JSON.stringify({ error: "userId and newUsername are required" }), { status: 400 });
+        }
+        if (!Types.ObjectId.isValid(userId)) {
+            return new NextResponse(JSON.stringify({ error: "Invalid userId" }), { status: 400 });
+        }
+
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: new ObjectId(userId) },
+            { username: newUsername },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return new NextResponse(JSON.stringify({ message: "User not found in the database" }), { status: 400 });
+        }
+
+
+        return new NextResponse(
+            JSON.stringify({ message: "User is updated", user: updatedUser }),
+            { status: 200 }
+        )
+
+
+    } catch (error: any) {
+        return new NextResponse("Error in fetching users: " + (error instanceof Error ? error.message : "Unknown error"), { status: 500 });
+
+
+
+
+    }
+}
 
 
 
